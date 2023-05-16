9894e7b0812f6ed9e30a9c2f24621c95f62942d2
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-05-16 09:17:15 -0300

    server inital setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
-import fastify from 'fastify'
+import fastify from "fastify"
+import {PrismaClient} from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello, World!'
+app.get("/users", async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('🚀 HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running on http://localhost:3333")
+  })
