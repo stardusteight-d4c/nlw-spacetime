6da2a53c4e50769ae8b94f4737046f5c2b0ad210
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-05-19 13:38:38 -0300

    upload server route and handle static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
 import "dotenv/config"
+import { resolve } from "node:path"
 import fastify from "fastify"
 import cors from "@fastify/cors"
 import jwt from "@fastify/jwt"
+import multipart from "@fastify/multipart"
+import fastifyStatic from "@fastify/static"
 import { memoriesRoutes } from "./routes/memories"
 import { authRoutes } from "./routes/auth"
+import { uploadRoutes } from "./routes/upload"
 
 const app = fastify()
 
+app.register(fastifyStatic, {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+})
+app.register(multipart)
 app.register(cors, {
   origin: true,
 })
@@ -15,11 +24,12 @@ app.register(jwt, {
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then((url) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+    console.log(`🚀 Server ready at ${url}`)
+  })
--- web/src/app/components/left-section/integrate/DefaultButton.tsx
+++ web/src/app/components/left-section/integrate/DefaultButton.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import React from "react"
 
 interface Props {
@@ -7,11 +8,11 @@ interface Props {
 
 export const DefaultButton = ({ title, href = "" }: Props) => {
   return (
-    <a
+    <Link
       className="font-alt inline-block rounded-full bg-green-500 px-5 py-3 text-sm uppercase leading-none text-black hover:bg-green-600"
       href={href}
     >
       {title}
-    </a>
+    </Link>
   )
 }
